702f34994c9551bce1efe359bf21c246a2c6b9ec
Morlax97 <miltontorasso@hotmail.com>
2019-05-06 15:56:31 +0000

    Multiple pin configuration

--- arduino/esp8266.ino
+++ arduino/esp8266.ino
@@ -3,21 +3,22 @@
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "";
-const char* password = "";
+const char* ssid     = "HF-Staff";
+const char* password = "S74ffW1F1";
 char* action = "action/device/1";
 char* estado = "status/device/1";
-char* server = "";
+char* server = "cherencio.henryford.esc.edu.ar";
 unsigned int bcastPort = 3000;
 unsigned int udpLocalPort = 2500;
-int LEDPin = 13; // D7
-int BotPin = 14; // D5
+int LEDPin; // 13 = D7
+int BotPin;// 14 = D5
 int override = 0;
 int configured = 0;
 char* bcastPacket = "{\"id\":1,\"type\":\"device\"}";
 char incomingPacket[255];
 char* bcastIP = "10.0.20.22";
 StaticJsonDocument<200> jsonDocument;
+JsonArray array;
 WiFiClient wifiClient;
 WiFiUDP Udp;
 PubSubClient client(wifiClient);
@@ -74,35 +75,42 @@ void setup() {
          }
        Serial.printf("UDP packet contents: %s\n", incomingPacket);
        auto error = deserializeJson(jsonDocument, incomingPacket);
+       array = jsonDocument.as<JsonArray>();
        if(error) {
          Serial.println("deserializeJson() failed\n");
          return;
          }
-       else{
-         const int pin = jsonDocument["pin"];
-         const String mode = jsonDocument["mode"];
-         if (mode.equals("OUTPUT")) {
-           Serial.printf("Configuring pin as output\n");
-           pinMode(pin, OUTPUT);
-           configured = 1;
+       else {
+         for(JsonVariant device : array) {
+           const int pin = device["pin"];
+           const String mode = device["mode"];
+           if (mode.equals("OUTPUT")) {
+             pinMode(pin, OUTPUT);
+             LEDPin = pin;
+             }
+           if (mode.equals("INPUT_PULLUP")) {
+             pinMode(pin, INPUT_PULLUP);
+             BotPin = pin;
+             }
            }
+         configured = 1;  
          }
        }
-     }
-}
+     } 
+  }
 
 
 void loop() {
-  delay(1000);
+  delay(100);
   if(!digitalRead(BotPin)) {
     digitalWrite(LEDPin, HIGH);
+    if (!override) {client.publish(estado,"1");}
     override = 1;
     }
   else if(digitalRead(BotPin) && override) {
     digitalWrite(LEDPin, LOW);
+    client.publish(estado,"0");
     override = 0;
     }
-  if(digitalRead(LEDPin)) {client.publish(estado,"1");}
-  else {client.publish(estado,"0");}
   client.loop();
   }
